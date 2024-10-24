5112b17484aeb20f1faaf73bd5c95914540731f4
NasserObm <nassermohamedobame@gmail.com>
2024-10-24 16:53:47 +0100

    Initial commit

---
+++ lib/welcome.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffD8EAEC),
+              Color(0xff0E4F57)
+            ], // Ajuste les couleurs du dégradé
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'TodoList',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
